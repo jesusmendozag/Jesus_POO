ab281971f1b74264d70104082d6c2e5da02e910e
Jesus <jesusmendozagomez6@gmail.com>
2023-12-03 13:39:07 -0600

    Reto final 0.3

--- Java/src/edu/yisus/RetoFinal/Tictactoe/Human.java
+++ Java/src/edu/yisus/RetoFinal/Tictactoe/Human.java
@@ -19,4 +19,3 @@ public class Human extends Player {
         return scanner.nextInt();
     }
 }
-
--- Java/src/edu/yisus/RetoFinal/Tictactoe/ScoreBoard.java
+++ Java/src/edu/yisus/RetoFinal/Tictactoe/ScoreBoard.java
@@ -5,10 +5,12 @@ import java.io.*;
 import java.util.*;
 
 public class ScoreBoard {
+    private static final String SCORES_FILE = "score.txt";
     private Map<String, Integer> scores;
 
     public ScoreBoard() {
         scores = new HashMap<>();
+        loadScoresFromFile();
     }
 
     public void addScore(String playerName, int score) {
@@ -28,6 +30,23 @@ public class ScoreBoard {
                     .forEach(entry -> writer.println(entry.getKey() + ": " + entry.getValue()));
         } catch (FileNotFoundException e) {
             System.out.println(LanguagesFactory.getMessage("error:score") + e.getMessage());
+        }
+    }
+
+    private void loadScoresFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(SCORES_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(": ");
+                if (parts.length == 2) {
+                    String playerName = parts[0];
+                    int score = Integer.parseInt(parts[1]); // Obtener puntaje como entero
+                    scores.put(playerName, score); // Agregar puntaje al mapa
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            // Manejar excepciones según sea necesario
+            e.printStackTrace();
         }
     }
 }
